fbdbe5fec1efc94c00647ef9a62d80bc53a22d46
XDzall <mghazalinurrahman939@gmail.com>
2025-06-01 16:01:12 +0700

    .

--- lib/screens/mahasiswa/profile_screen.dart
+++ lib/screens/mahasiswa/profile_screen.dart
@@ -6,7 +6,7 @@ import 'package:mobile_mis_mahasiswa/screens/auth/login_screen.dart';
 import 'package:mobile_mis_mahasiswa/widgets/custom_navbar.dart';
 
 class ProfileScreen extends StatefulWidget {
-  const ProfileScreen({Key? key}) : super(key: key);
+  const ProfileScreen({super.key});
 
   @override
   State<ProfileScreen> createState() => _ProfileScreenState();
